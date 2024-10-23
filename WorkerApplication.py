c7c28373e22c34a3f49a7e8b4e398423f4e8a7d5
TalkinWhip <vmolaliyski@tutanota.com>
2024-10-23 13:30:23 +0200

    exercise6 with current python versions

--- WorkerApplication.py
+++ WorkerApplication.py
@@ -5,17 +5,27 @@ from pyzeebe import ZeebeClient, ZeebeWorker, ZeebeTaskRouter, create_camunda_cl
 # Setup logging
 # logging.basicConfig(level=logging.Debug)
 
+def get_customer_credit(customer_id: str):
+    return customer_id[-2:]
+
+def deduct_credit(customer_id: str, amount: float):
+    open_amount = amount - float(get_customer_credit(customer_id))
+    return open_amount
+
 # Define tasks
 router = ZeebeTaskRouter()
 
 @router.task("credit-deduction")
-def deduct_credit(job: Job):
+def deduct_credit(job: Job, customerId: str, orderTotal: float):
     print(f"Handling job: {job.type}")
-    return
+    open_amount = deduct_credit(customerId, orderTotal)
+    customer_credit = get_customer_credit(customerId)
+    return {'openAmount': open_amount, 'customerCredit': customer_credit}
 
-@router.task("credit-card-charging")
+@router.task("credit-card-charging", cardNumber: str, cvc: int, expiryDate: )
 def credit_card_charge(job: Job):
     print(f"Handling job: {job.type}")
+    print("Charging credit card with number " + cardNumber + ", cvc " + cvc + ", expiry date " + expiryDate)
     return
 
 # Create a channel, the worker and include the router with tasks
