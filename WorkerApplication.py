8b3e730f12ed00b2fb6e3e6905d67bd532415326
TalkinWhip <vmolaliyski@tutanota.com>
2024-11-26 10:48:30 +0100

    rework of e6 with main function

--- WorkerApplication.py
+++ WorkerApplication.py
@@ -1,13 +1,9 @@
 import asyncio
+import logging
+from pyzeebe import ZeebeClient, ZeebeWorker, ZeebeTaskRouter, create_camunda_cloud_channel, Job
 
-from pyzeebe import ZeebeClient, ZeebeWorker, create_camunda_cloud_channel, Job
-
-grpc_channel = create_camunda_cloud_channel(client_id="",
-                                            client_secret="",
-                                            cluster_id="",
-                                            region="")
-zeebe_client = ZeebeClient(grpc_channel)
-worker = ZeebeWorker(grpc_channel)
+# Setup logging
+# logging.basicConfig(level=logging.Debug)
 
 def get_customer_credit(customer_id: str):
     return customer_id[-2:]
@@ -16,21 +12,30 @@ def deduct_credit(customer_id: str, amount: float):
     open_amount = amount - float(get_customer_credit(customer_id))
     return open_amount
 
-@worker.task("credit-deduction")
+# Define tasks
+router = ZeebeTaskRouter()
+
+@router.task("credit-deduction")
 def handle_credit_deduction(job: Job, customerId: str, orderTotal: float):
-    print("Handling job: " + job.type)
+    print(f"Handling job: {job.type}")
     open_amount = deduct_credit(customerId, orderTotal)
     customer_credit = get_customer_credit(customerId)
     return {'openAmount': open_amount, 'customerCredit': customer_credit}
 
-@worker.task("credit-card-charging")
-def handle_credit_card_charging(job: Job, cardNumber: str, cvc: str, expiryDate: str):
-    print("Handling job: " + job.type)
+@router.task("credit-card-charging")
+def handle_credit_card_charging(job: Job, cardNumber: str, cvc: int, expiryDate: str):
+    print(f"Handling job: {job.type}")
     print("Charging credit card with number " + cardNumber + ", cvc " + cvc + ", expiry date " + expiryDate)
     return
 
+# Create a channel, the worker and include the router with tassks
+async def main():
+    grpc_channel = create_camunda_cloud_channel(client_id="xxx",
+                                                client_secret="xxx",
+                                                cluster_id="xxx",
+                                                region="bru-2")
+    worker = ZeebeWorker(grpc_channel)
+    worker.include_router(router)
+    await worker.work()
 
-asyncio.get_event_loop().run_until_complete(worker.work())
-
-
-
+asyncio.run(main())
